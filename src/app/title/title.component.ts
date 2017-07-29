9570d5c9e152966d193aec6ed24e28d38a03fa6f
qo4rmp1 <qo4rmp1@gmail.com>
2017-07-29 11:00:55 +0800

    任務 04：套用內嵌繫結 (Interpolation)

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TitleComponent implements OnInit {
   constructor() { }
-  title: 'todos';
+  title= 'todos';
 
   ngOnInit() {
   }
