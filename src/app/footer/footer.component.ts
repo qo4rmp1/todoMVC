86cec4e8336e6fb4a9ebac18202a9a103c64d549
qo4rmp1 <qo4rmp1@gmail.com>
2017-07-29 14:28:16 +0800

    任務 10進階3移除 ngOnChanges() 方法，改用 TypeScript 的 getter/setter 來自訂 todos 的傳入方式：套用 @Input() 裝飾器 (Decorator)

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -5,9 +5,19 @@ import { Component, Input, Output, OnInit, EventEmitter, OnChanges } from '@angu
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit, OnChanges {
+export class FooterComponent implements OnInit {
 
-  @Input('data') todos;
+  private _todos: any[];
+
+
+  @Input('data')
+  set todos(todos) {
+    this._todos = todos;
+    this.tooMore = this.todos.length > 5
+  }
+  get todos() {
+    return this._todos;
+  }
 
   @Output()
   clearcompleted = new EventEmitter<any>();
@@ -22,11 +32,4 @@ export class FooterComponent implements OnInit, OnChanges {
   }
 
   tooMore;
-  ngOnChanges() {
-    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
-    //Add 'implements OnChanges' to the class.
-    console.log('ngOnChanges');
-    // this.tooMore = this.todosLength > 5;
-    this.tooMore = this.todos.length > 5
-  }
 }
