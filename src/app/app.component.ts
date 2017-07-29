f96a7876cf15579aba66c6d05510602fea57b1fb
qo4rmp1 <qo4rmp1@gmail.com>
2017-07-29 17:14:18 +0800

    任務 15：使用 Http 服務元件呼叫 RESTful APIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 
 @Component({
@@ -6,47 +6,50 @@ import { Http } from '@angular/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   colspan = 2;
   title = 'app';
   inputHint = 'What needs to be done?';
   todos: any[] = [];
   todo: string = '';
+  private apiUrl = 'http://localhost:3000/todos';
 
   constructor(private http:Http) {
 
   }
 
+  ngOnInit() {
+     this.http.get(this.apiUrl)
+    .subscribe(res=> {
+      this.todos = res.json();
+    })
+  }
+
   addTodo() {
     if (this.todo) {
-      //這邊要注意改成[...this.todos];,再變更陣列時,才會觸發變更偵測
-      this.todos = [...this.todos];
-      this.todos.push({
+      let newTodo = {
         text: this.todo,
       done: false
-      });
-      this.todo = '';
+      };
+      //這邊要注意改成[...this.todos];,再變更陣列時,才會觸發變更偵測
+      // this.todos = [...this.todos];
+      // this.todos.push(newTodo);
+      // this.todo = '';
 
-    //   this.todos.push({
-    //     text: this.todo,
-    //   done: false});
-    //   this.todo = '';
+      this.http.post(this.apiUrl, newTodo)
+      .subscribe(res => {
+        let data = res.json();
+        this.todos = [...this.todos];
+        this.todos.push(data);
+        this.todo = '';
+      });
     }
   }
 
   clearcompleted(evt) {
-    this.todos = this.todos.filter(item => !item.done);
-    // for(let i = this.todos.length - 1; i >= 0; i--) {
-    //   if (this.todos[i].done){
-    //     this.todos.splice(i, 1);
-    //   }
-    // }
-    // this.todos.forEach(element => {
-    //   if (element.done){
-    //     let idx = this.todos.indexOf(element);
-    //     this.todos.splice(idx, 1);
-    //   }
-    // });
+    this.todos = this.todos.filter(item => item.done).map(item => {
+    this.removeTodo(item);
+    });
   }
 
   filterType;
@@ -59,11 +62,23 @@ export class AppComponent {
   isToggleAll: boolean = false;
   toggleAll(event) {
     this.todos = this.todos.map(item=>{item.done = event; return item;});
+
+    this.todos.forEach(item=>{
+      this.http.put(`${this.apiUrl}/${item.id}`, item).subscribe();
+    })
   }
 
   removeTodo(todo) {
     console.log(todo);
-    this.todos = this.todos.filter(items=> items !== todo);
-    // this.todos = [...this.todos];
+      this.todos = this.todos.filter(items=> items !== todo);
+      this.http.delete(`${this.apiUrl}/${todo.id}`)
+      .subscribe(res => {
+        this.todos = this.todos.filter(item=>item.id != todo.id);
+      });
   }
+
+  todoUpdate(todo) {
+      this.http.put(`${this.apiUrl}/${todo.id}`, todo).subscribe();
+  }
+
 }
